df648d235be53fbd64690195c14751ec6709c976
Khoa Le <khoa.le@hrboss.com>
2016-06-15 17:48:09 +0700

    Remove old data

--- index.md
+++
@@ -1,5 +0,0 @@
----
-id: home
----
-
-{% include index.html  %}
--- js/polyfill.js
+++
@@ -1,1 +0,0 @@
-"function"!=typeof Function.prototype.bind&&(Function.prototype.bind=function(t){var o=Array.prototype.slice.call(arguments,1),n=this,r=function(){},p=function(){return n.apply(this instanceof r?this:t||{},o.concat(Array.prototype.slice.call(arguments)))};return r.prototype=this.prototype||{},p.prototype=new r,p});
