018993e0d9ddd169bb07d2d885107b115ec3658c
Daniele Lolli (UncleDan) <git@uncledan.it>
2025-12-29 16:40:09 +0100

    * mysql ( https://dev.mysql.com/downloads/mysql/ ) ==> mysql-8.0.44-winx64 _(deleted PDB files)_ (DOWNGRADE!)

---
+++ bin/mysql/mysql-8.0.44-winx64/include/mysql_version.h
@@ -0,0 +1,32 @@
+/* Copyright Abandoned 1996,1999 TCX DataKonsult AB & Monty Program KB
+   & Detron HB, 1996, 1999-2004, 2007 MySQL AB.
+   This file is public domain and comes with NO WARRANTY of any kind
+*/
+
+/* Version numbers for protocol & mysqld */
+
+#ifndef _mysql_version_h
+#define _mysql_version_h
+
+#define PROTOCOL_VERSION            10
+#define MYSQL_SERVER_VERSION       "8.0.44"
+#define MYSQL_BASE_VERSION         "mysqld-8.0"
+#define MYSQL_SERVER_SUFFIX_DEF    ""
+#define MYSQL_VERSION_ID            80044
+#define MYSQL_VERSION_STABILITY     "LTS"
+#define MYSQL_PORT                  3306
+#define MYSQL_ADMIN_PORT            33062
+#define MYSQL_PORT_DEFAULT          0
+#define MYSQL_UNIX_ADDR            "/tmp/mysql.sock"
+#define MYSQL_CONFIG_NAME          "my"
+#define MYSQL_PERSIST_CONFIG_NAME  "mysqld-auto"
+#define MYSQL_COMPILATION_COMMENT  "MySQL Community - GPL"
+#define MYSQL_COMPILATION_COMMENT_SERVER  "MySQL Community Server - GPL"
+#define LIBMYSQL_VERSION           "8.0.44"
+#define LIBMYSQL_VERSION_ID         80044
+
+#ifndef LICENSE
+#define LICENSE                     GPL
+#endif /* LICENSE */
+
+#endif /* _mysql_version_h */
